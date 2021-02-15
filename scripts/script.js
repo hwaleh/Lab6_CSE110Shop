25f5c9363309cb7c293a75f5fd32a49dcc8b7176
hwaleh <hwaleh@ucsd.edu>
2021-02-14 21:39:18 -0800

    fetch

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,23 @@ window.addEventListener('DOMContentLoaded', () => {
     .then(response => response.json())
     .catch(error=>console.log(error))
   //console.log(response);*/
-
-  fetch('https://fakestoreapi.com/products')
-  .then(response =>localStorage.setItem('products', JSON.stringify(response)))
+  if(localStorage.getItem('products')==null){
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      //localStorage.setItem('items',data)
+      //console.log(typeof data)
+      localStorage.setItem('products',JSON.stringify(data))
+      /*for (product of data){
+        console.log(product)
+      
+      }*/
+    })
+    .catch(console.error);
+  }
+  
   //.then(data => localStorage.setItem('item',))
   
 
-  .catch(console.error);
+  
 });
