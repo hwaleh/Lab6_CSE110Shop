46b350568badffaaf46140e371f3a7b8de2cb95e
hwaleh <hwaleh@ucsd.edu>
2021-02-14 23:45:54 -0800

    almost

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,28 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor(){
+    super();
+    let template = document.getElementById('product-item');
+    //let templateContent = template.content;
+
+    const shadowRoot = this.attachShadow({mode: 'open'});
+    shadowRoot.innerHTML = `
+      <style> <link rel="stylesheet" href="./styles/styles.css"> </style>
+      <li class="product">
+        <img id="pimg" src="" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+        <p class="title" value=""></p>
+        <p class="price" value=""></p>
+        <button onclick="alert('Added to Cart!')">Add to Cart</button>
+       </li>
+    `;
+    
+    /*const shadowRoot = this.attachShadow({mode: 'open'})
+      .appendChild(templateContent.cloneNode(true));*/
+  }
+  /*setTitle(string){
+    this.setTitle(string)
+  }*/
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,22 @@ window.addEventListener('DOMContentLoaded', () => {
     .then(data => {
       //localStorage.setItem('items',data)
       //console.log(typeof data)
+      //console.log(data)
       localStorage.setItem('products',JSON.stringify(data))
+      let arr=JSON.parse(localStorage.getItem('products'))
+      
+      for(product of arr){
+        console.log(typeof product.title)
+        let tmpl = document.createElement('product-item')
+        tmpl.title=product.title
+        tmpl.price=product.price
+        document.getElementById("product-list").append(tmpl)
+        
+        //tmpl.pimg.src=product.image
+        
+        //document.createElement()
+        //console.log(product.title)
+      }
       /*for (product of data){
         console.log(product)
       
