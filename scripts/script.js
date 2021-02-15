7cacc5ff25c442a028a959ecd1b866ad4dcbe14d
hwaleh <hwaleh@ucsd.edu>
2021-02-14 20:05:24 -0800

    fetch

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,9 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .catch(error=>console.log(error))
+  //console.log(response);
+
 });
